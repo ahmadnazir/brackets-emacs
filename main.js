327daa3e236f37b3507f5e896f1becb94d4eb3e6
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2014-03-06 23:45:03 +0100

    Added the override commands i.e. commands that do not have an user defined handler.

--- main.js
+++ main.js
@@ -6,11 +6,14 @@ define(function (require, exports, module) {
     var EditorManager       = brackets.getModule("editor/EditorManager"),
         CommandManager      = brackets.getModule("command/CommandManager"),
         Menus               = brackets.getModule("command/Menus"),
+        Commands            = brackets.getModule("command/Commands"),
         KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
         AppInit             = brackets.getModule("utils/AppInit"),
+        
         // Text Selection Ring
         ring        = [],
         ringLength  = 30,
+        
         // Mark
         isMarkSet   = false;
      
@@ -23,11 +26,11 @@ define(function (require, exports, module) {
     function moveEndOfLine() {
         var editor      = EditorManager.getFocusedEditor(),
             cursorPos   = editor.getCursorPos();
-        //@todo: don't have a way to get the current line
+        // @todo: don't have a way to get the current line
         // This is a crude way to get to the end of line
         editor.setCursorPos(cursorPos.line, 1000);
     }
-    
+
     function setMarkCommand() {
         isMarkSet = !isMarkSet;
         
@@ -66,72 +69,83 @@ define(function (require, exports, module) {
 
     AppInit.appReady(function () {
 
-        var commands, menus;
+        var menus = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU),
+            
+            // Command Ids
+            MOVE_BEGINNING_OF_LINE = "emacs.move-beginning-of-line",
+            MOVE_END_OF_LINE = "emacs.move-end-of-line",
+            YANK = "emacs.yank",
+            SET_MARK_COMMAND = "emacs.set-mark-command",
+            KILL_REGION = "emacs.kill-region",
+            KILL_RING_SAVE = "emacs.kill-ring-save",
 
-        /*
-         * Emacs commands
-         *
-         * Find list of commands in emacs: Ctrl-h b
-         */
+            /*
+             * Emacs commands
+             *
+             * Find list of commands in emacs: Ctrl-h b
+             */
+            commands = [
+                {
+                    id: MOVE_BEGINNING_OF_LINE,
+                    name: "Move Beginning of Line",
+                    keyBinding: "Ctrl-A",
+                    callback: moveBeginningOfLine
+                },
+                {
+                    id: MOVE_END_OF_LINE,
+                    name: "Move End of Line",
+                    keyBinding: "Ctrl-E",
+                    callback: moveEndOfLine
+                },
+                {
+                    id: YANK,
+                    name: "Yank",
+                    keyBinding: "Ctrl-Y",
+                    callback: yank
+                },
+                {
+                    id: SET_MARK_COMMAND,
+                    name: "Set Mark Command",
+                    keyBinding: "Ctrl-Space",
+                    callback: setMarkCommand
+                },
+                {
+                    id: KILL_REGION,
+                    name: "Kill Region",
+                    keyBinding: "Ctrl-W",
+                    callback: killRegion
+                },
+                {
+                    id: KILL_RING_SAVE,
+                    name: "Kill Ring Save",
+                    keyBinding: "Alt-W",
+                    callback: killRingSave
+                }
+            ],
         
-        commands = [
-            {
-                name: "Move Beginning of Line",
-                keyBinding: "Ctrl-A",
-                callback: moveBeginningOfLine
-            },
-            {
-                name: "Move End of Line",
-                keyBinding: "Ctrl-E",
-                callback: moveEndOfLine
-            },
-            {
-                name: "Yank",
-                keyBinding: "Ctrl-Y",
-                callback: yank
-            },
-            {
-                name: "Set Mark Command",
-                keyBinding: "Ctrl-Space",
-                callback: setMarkCommand
-            },
-            {
-                name: "Kill Ring Save",
-                keyBinding: "Alt-W",
-                callback: killRingSave
-            },
-            {
-                name: "Kill Region",
-                keyBinding: "Ctrl-W",
-                callback: killRegion
-
-            }
-
-
-        ];
+            // override commands
+            overrideCommands = [
+                {
+                    id: Commands.EDIT_UNDO,
+                    keyBinding: "Ctrl-/"
+                }
+            ];
         
-        menus = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
-        
+        function remap(item) {
+            KeyBindingManager.removeBinding(item.keyBinding);
+            KeyBindingManager.addBinding(item.id, item.keyBinding);
+            menus.addMenuItem(item.id);
+        }
+                
         commands.forEach(function (item, index) {
-            CommandManager.register(item.name, item.name, item.callback);
+            CommandManager.register(item.name, item.id, item.callback);
         });
 
-        menus.addMenuItem(Menus.DIVIDER);
-
-        window.setTimeout(function () { // @todo: because the keybindingmodule hasn't loaded yet.
-            commands.forEach(function (item, index) {
-                KeyBindingManager.removeBinding(item.keyBinding);
-                menus.addMenuItem(item.name, [{key: item.keyBinding}]);
-            });
+        window.setTimeout(function () { // @todo: keybinding module takes some time to load
+            overrideCommands.forEach(remap);
+            menus.addMenuItem(Menus.DIVIDER);
+            commands.forEach(remap);
         }, 500);
-        
-//        
-//        CommandManager.register(Strings.CMD_CUT,              Commands.EDIT_CUT,              ignoreCommand);
-//        CommandManager.register(Strings.CMD_COPY,             Commands.EDIT_COPY,             ignoreCommand);
-//        CommandManager.register(Strings.CMD_PASTE,            Commands.EDIT_PASTE,            ignoreCommand);
-
-        //log("Brackets EMACS Keybindings extension loaded.");
- 
+         
     });
- 
 });
